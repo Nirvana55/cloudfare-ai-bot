8e86c568917d901e8d71d26ee9b876ab58895c88
Nirvana55 <lamabruno55@gmail.com>
2024-04-11 18:18:11 +0545

    refactor: remove default route redirection on default

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/auth/login",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
