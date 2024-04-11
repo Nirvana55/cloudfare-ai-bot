f3bb1ba7ba6bf0c732cb2648ad767eca0f657c43
Nirvana55 <lamabruno55@gmail.com>
2024-04-11 18:22:15 +0545

    refactor: update route handle for insert of chat and chat message data

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,13 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  let redirectId;
   try {
-    const { question } = await req.json();
+    const { question, user_id, isInitialChat, message_chat_id } = await req
+      .json();
+    const supabase = createClient();
     const cloudflareResponse = await fetch(
       `https://api.cloudflare.com/client/v4/accounts/${process.env.NEXT_CLOUD_FARE_API_USER_ID}/ai/run/@hf/thebloke/mistral-7b-instruct-v0.1-awq`,
       {
@@ -18,8 +23,37 @@ export async function POST(req: Request) {
     );
 
     const data = await cloudflareResponse.json();
-    return NextResponse.json(data);
-  } catch (error) {
-    return new Response("Error fetching from Cloudflare", { status: 500 });
+
+    if (!isInitialChat) {
+      const { error } = await supabase.from("messages").insert({
+        user_id,
+        prompt: question,
+        chat_id: message_chat_id,
+        reply: data.result.response,
+      });
+
+      if (error) {
+        throw error;
+      }
+
+      return NextResponse.json(true);
+    }
+    const { data: chat_id } = await supabase.rpc(
+      "create_chat_and_chat_message",
+      {
+        _user_id: user_id,
+        _prompt: question,
+        _reply: data.result.response,
+      },
+    );
+    redirectId = chat_id;
+  } catch (error: any) {
+    console.error("Error in POST handler:", error); // Log the error
+    return new Response("An error occurred", { status: 500 });
+  }
+  if (
+    redirectId
+  ) {
+    redirect(`/dashboard/${redirectId}`);
   }
 }
