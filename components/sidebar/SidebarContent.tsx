6090bb4911e10c0553be08a9d1a98bfe8446a4a9
Nirvana55 <lamabruno55@gmail.com>
2024-04-15 07:31:54 +0545

    fix: sidebar delete selected item

--- components/sidebar/SidebarContent.tsx
+++ components/sidebar/SidebarContent.tsx
@@ -29,7 +29,8 @@ const SidebarContent = (props: SideBarContentProps) => {
 
   const deleteChatHistory = useDeleteChatHistory();
 
-  const handleChatHistory = () => deleteChatHistory.mutate(Number(id));
+  const handleChatHistory = (chat_id: number) =>
+    deleteChatHistory.mutate(Number(chat_id));
   return (
     <>
       <div className='flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6'>
@@ -43,42 +44,45 @@ const SidebarContent = (props: SideBarContentProps) => {
       </div>
       <div className='hover:overflow-y-auto lg:h-[calc(100%-56px)]'>
         <div className='mt-3 items-start px-2 text-sm font-medium lg:px-4'>
-          <Dialog>
-            {chatData?.map((item) => (
-              <Link
-                key={item.chat_id}
-                href={`/dashboard/${item.chat_id}`}
-                className={`flex mb-2`}
+          {chatData?.map((item, index) => (
+            <Link
+              key={item.chat_id}
+              href={`/dashboard/${item.chat_id}`}
+              className={`flex mb-2`}
+            >
+              <div
+                className={`flex gap-2 items-center cursor-pointer px-2 py-2 group ${
+                  item.chat_id === Number(id)
+                    ? "bg-black dark:bg-zinc-800 rounded-md text-white isSelected"
+                    : "hover:bg-black dark:hover:bg-zinc-800 hover:text-white isNotSelected rounded-md"
+                }`}
               >
-                <div
-                  className={`flex gap-2 items-center cursor-pointer px-2 py-2 group ${
-                    item.chat_id === Number(id)
-                      ? "bg-black dark:bg-zinc-800 rounded-md text-white isSelected"
-                      : "hover:bg-black dark:hover:bg-zinc-800 hover:text-white isNotSelected rounded-md"
-                  }`}
-                >
-                  <MessageSquare size={16} />
-                  <div className='flex-1 w-[140px] group-[.isNotSelected]:w-[160px] lg:w-[180px] lg:group-[.isNotSelected]:w-[205px]'>
-                    <p className='truncate text-[14px] capitalize'>
-                      {item.messages[0].prompt}
-                    </p>
-                  </div>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <EllipsisVertical
-                        size={16}
-                        className='justify-end cursor-pointer hidden group-[.isSelected]:block'
-                      />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className='w-[20px]' align='center'>
-                      <DialogTrigger asChild>
-                        <DropdownMenuItem className='cursor-pointer text-red-500 flex items-center justify-center gap-2'>
-                          <Trash2 size={14} />
-                          <p>Delete</p>
-                        </DropdownMenuItem>
-                      </DialogTrigger>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
+                <MessageSquare size={16} />
+                <div className='flex-1 w-[140px] group-[.isNotSelected]:w-[160px] lg:w-[180px] lg:group-[.isNotSelected]:w-[205px]'>
+                  <p className='truncate text-[14px] capitalize'>
+                    {item.messages[0].prompt}
+                  </p>
+                </div>
+                <Dialog>
+                  {item.chat_id === Number(id) && (
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <EllipsisVertical
+                          size={16}
+                          className='justify-end cursor-pointer hidden group-[.isSelected]:block'
+                        />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className='w-[20px]' align='center'>
+                        <DialogTrigger asChild>
+                          <DropdownMenuItem className='cursor-pointer text-red-500 flex items-center justify-center gap-2'>
+                            <Trash2 size={14} />
+                            <p>Delete</p>
+                          </DropdownMenuItem>
+                        </DialogTrigger>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  )}
+
                   <DialogContent>
                     <DialogHeader>
                       <DialogTitle>Delete chat?</DialogTitle>
@@ -89,7 +93,7 @@ const SidebarContent = (props: SideBarContentProps) => {
                     <DialogFooter>
                       <Button
                         className='bg-red-600 text-white font-semibold hover:bg-red-700'
-                        onClick={handleChatHistory}
+                        onClick={() => handleChatHistory(item.chat_id)}
                         disabled={deleteChatHistory.isPending}
                         type='submit'
                       >
@@ -97,10 +101,10 @@ const SidebarContent = (props: SideBarContentProps) => {
                       </Button>
                     </DialogFooter>
                   </DialogContent>
-                </div>
-              </Link>
-            ))}
-          </Dialog>
+                </Dialog>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </>
